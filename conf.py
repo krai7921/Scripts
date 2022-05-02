a35adde2b54c95667dd3aae844b8dbd31b48c98a
krai7921 <103641262+krai7921@users.noreply.github.com>
2022-05-03 00:25:58 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,30 +1,13 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
 # -- Project information -----------------------------------------------------
 import os
 from pathlib import Path
 from urllib import request
 
-
-
 project = 'OPTUM SOC DMS'
 copyright = '2022, KRAI'
 author = 'KRAI'
+
+# language = "fr"  # For testing language translations
 
 master_doc = "index"
 
@@ -34,7 +17,7 @@ master_doc = "index"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-   "ablog",
+    "ablog",
     "myst_nb",
     "numpydoc",
     "sphinx.ext.autodoc",
@@ -51,12 +34,12 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3.8", None),
@@ -84,8 +67,6 @@ myst_enable_extensions = [
     # "substitution",
 ]
 
-
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
@@ -110,12 +91,11 @@ html_sidebars = {
         "archives.html",
         "sbt-sidebar-nav.html",
     ]
-}'
-
+}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 jupyter_execute_notebooks = "cache"
 thebe_config = {
     "repository_url": "https://github.com/binder-examples/jupyter-stacks-datascience",
@@ -144,7 +124,15 @@ html_theme_options = {
         "⚠️The latest release refactored our HTML, "
         "so double-check your custom CSS rules!⚠️"
     ),
+    # For testing
+    # "use_fullscreen_button": False,
+    # "home_page_in_toc": True,
+    # "single_page": True,
+    # "extra_footer": "<a href='https://google.com'>Test</a>",  # DEPRECATED KEY
+    # "extra_navbar": "<a href='https://google.com'>Test</a>",
+    # "show_navbar_depth": 2,
 }
+
 # -- ABlog config -------------------------------------------------
 blog_path = "reference/blog"
 blog_post_pattern = "reference/blog/*.md"
@@ -204,6 +192,3 @@ def setup(app):
             "https://assets.readthedocs.org/static/javascript/readthedocs-doc-embed.js",
             priority=501,
         )
-© 2022 GitHub, Inc.
-Terms
-Privacy
