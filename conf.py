4e8fe38a5e7e1683c62955387ecf4d484da8c7c4
krai7921 <103641262+krai7921@users.noreply.github.com>
2022-05-03 00:39:16 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -23,7 +23,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.intersphinx",
     "sphinx.ext.viewcode",
-    "sphinxcontrib.youtube",
+   # "sphinxcontrib.youtube",
     "sphinx_copybutton",
     "sphinx_design",
     "sphinx_tabs.tabs",
